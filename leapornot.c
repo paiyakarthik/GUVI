1cf3f363769b8c8fe7fdf9079fc77bd9b5dcb475
karthik <karthik27061995@gmail.com>
2016-10-20 16:59:12 +0530

    Add files via upload

---
+++ leapornot.c
@@ -0,0 +1,19 @@
+
+
+#include <stdio.h>
+ 
+int main()
+{
+  int year;
+ 
+  printf("Enter a year to check if it is a leap year\n");
+  scanf("%d", &year);
+ 
+  if ( year%400 == 0)
+    printf("%d is a leap year", year);
+  
+  else
+    printf("%d is not a leap year", year);  
+ 
+  return 0;
+}
