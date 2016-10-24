3d8fbd430082e644363c72367f5dfb6014390abb
karthik <karthik27061995@gmail.com>
2016-10-24 10:10:47 +0530

    Add files via upload

---
+++ alphaornot.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char b;
+printf("enter the  char");
+scanf("%c",&b);
+if(s>='a'&&c<='z')||(s=>'A'&& s<='Z'))
+printf("%c is the alphabet",s);
+else
+printf("%c is the not alphabet",s);
+getch();
+}
